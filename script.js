ed6182376fe81ea92583cc240bb846fc6d2e27fa
Javier <javier.saurin.martinez@gmail.com>
2026-02-10 21:28:43 +0100

    fix: el numero se genera solo y en consola aparece

--- script.js
+++ script.js
@@ -6,4 +6,21 @@ let botonEnviar = document.querySelector(".enviar");
 let botonReinicio = document.querySelector(".reinicio");
 let mensajes = document.getElementById("mensajes");
 let numeroIntentos = document.querySelector(".numeroIntentos");
+let botonEmpezar = document.querySelector(".Empezar");
+
+// Logica del juego:
+// 1.Botón para empezar el juego
+let juegoEmpezado = false;
+numeroSecreto = true;
+botonEmpezar.addEventListener("click", function() {
+    if (juegoEmpezado === false) {
+        // 2.Generar el número secreto
+        let numeroSecreto = Math.floor(Math.random() * 100) + 1;
+        console.log(numeroSecreto);
+    } else {
+        juegoEmpezado === false;
+        numeroSecreto === false;
+    }
+})
+
 
