93fc403505154123528cbcbf26c47f843103c4ad
Javier <javier.saurin.martinez@gmail.com>
2026-02-10 20:42:09 +0100

    fix: variables recogidas en js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Recogemos en variables todas las clases necesarias
+
+let numeroAdivinar = document.querySelector(".numeroAdivinar");
+let input = document.querySelector(".insertarNumero");
+let botonEnviar = document.querySelector(".enviar");
+let botonReinicio = document.querySelector(".reinicio");
+let mensajes = document.getElementById("mensajes");
+let numeroIntentos = document.querySelector(".numeroIntentos");
+
