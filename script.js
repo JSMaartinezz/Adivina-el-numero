063bef7d0dbbb4aa7628eb487f906457fa383a04
Javier <javier.saurin.martinez@gmail.com>
2026-02-12 18:35:31 +0100

    fix: numero de intentos en consola

--- script.js
+++ script.js
@@ -5,19 +5,21 @@ let input = document.querySelector(".insertarNumero");
 let botonEnviar = document.querySelector(".enviar");
 let botonReinicio = document.querySelector(".reinicio");
 let mensajes = document.getElementById("mensajes");
-let numeroIntentos = document.querySelector(".numeroIntentos");
+
 let botonEmpezar = document.querySelector(".Empezar");
 
 // Logica del juego:
 // 1.Botón para empezar el juego
 let juegoEmpezado = false;
- let numeroSecreto;
+let numeroSecreto;
+let numeroIntentos = document.querySelector(".numeroIntentos");
 botonEmpezar.addEventListener("click", function() {
     if (juegoEmpezado === false) {
         // 2.Generar el número secreto
         numeroSecreto = Math.floor(Math.random() * 100) + 1;
         console.log(numeroSecreto);
-        juegoEmpezado = true
+        juegoEmpezado = true;
+        numeroIntentos = 0;
         numeroAdivinar.textContent = ("Número generado, escribe un numero debajo y pulsa enviar para intentar adivinarlo.");
     };
 });
@@ -26,10 +28,14 @@ botonEnviar.addEventListener("click", function() {
     let inputNumero = Number(input.value);
     if (juegoEmpezado === false) {
         mensajes.textContent = ("Pulsa: EMPEZAR")
-    }
-   else if (inputNumero < numeroSecreto) {
+    } else {
+        numeroIntentos ++;
+        console.log(numeroIntentos);
+    if (inputNumero < numeroSecreto) {
         mensajes.textContent = ("MAS ALTO");
     } else if (inputNumero > numeroSecreto) {
         mensajes.textContent = ("MAS BAJO");
-    } else {mensajes.textContent = ("¡HAS ACERTADO!")};
+    } else {mensajes.textContent = ("¡HAS ACERTADO!") 
+    }};
 });
+
